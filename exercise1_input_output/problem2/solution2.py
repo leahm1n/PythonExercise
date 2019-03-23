ebf472de86476e1079817c2b8faf0ae743df0de0
miatyann <miasann@163.com>
2019-03-23 22:00:13 +0800

    修改程序中的一些无意义变量名字的使用

--- exercise1_input_output/problem2/solution2.py
+++ exercise1_input_output/problem2/solution2.py
@@ -2,16 +2,14 @@ name = input('你的名字？')
 age = input('你的年龄？')
 number = input('你的学号？')
 email = input('你的邮箱？')
-x = '名字'+':'+name
-y = '年龄'+':'+age
-m = '学号'+':'+number
-h = '邮箱'+':'+email
 
-print(x)
-print(y)
-print(m)
-print(h)
 
+#程序运行结果正确，但是程序里使用了一些如x, y, m, h之类的变量名来存储要打印的字符串
+#变量名字要起得有含义写，比如name_str, age_str, email_str之类的。
+print('名字'+':'+name)
+print('年龄'+':'+age)
+print('学号'+':'+number)
+print('邮箱'+':'+email)
 
 tall = input('你的身高？')
 wegiht = input('你的体重？')

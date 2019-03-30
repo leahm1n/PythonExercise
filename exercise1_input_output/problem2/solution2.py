56315f6d9fccc3b3be2b97efc4bdeff79b5363ae
miatyann <miasann@163.com>
2019-03-30 18:56:05 +0800

    增加列表list的练习

--- exercise1_input_output/problem2/solution2.py
+++ exercise1_input_output/problem2/solution2.py
@@ -5,7 +5,7 @@ email = input('你的邮箱？')
 
 
 #程序运行结果正确，但是程序里使用了一些如x, y, m, h之类的变量名来存储要打印的字符串
-#变量名字要起得有含义写，比如name_str, age_str, email_str之类的。
+#变量名字要起得有含义些，比如name_str, age_str, email_str之类的。
 print('名字'+':'+name)
 print('年龄'+':'+age)
 print('学号'+':'+number)
